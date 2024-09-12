7983325632c006edf95f648ad99203e750fdf2ea
jcortez98 <jccortez98@gmail.com>
2024-09-12 16:38:41 -0600

    initial commit

---
+++ src/main/java/com/example/git/schoolmanagmentsystem/SchoolmanagmentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.git.schoolmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagmentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagmentsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git/schoolmanagmentsystem/SchoolmanagmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git.schoolmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmanagmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
